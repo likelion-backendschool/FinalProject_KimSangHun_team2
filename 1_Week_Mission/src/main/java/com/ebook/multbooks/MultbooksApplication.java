a159923981de17ba7ef476583169aca7da5753fe
SangHun <shzero211@naver.com>
2022-10-17 11:35:10 +0900

    1_Week 관련 디렉토리 및 파일 추가

---
+++ 1_Week_Mission/src/main/java/com/ebook/multbooks/MultbooksApplication.java
@@ -0,0 +1,13 @@
+package com.ebook.multbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultbooksApplication.class, args);
+	}
+
+}
