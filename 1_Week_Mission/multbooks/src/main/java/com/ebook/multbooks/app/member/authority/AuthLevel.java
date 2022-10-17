d472f8c483493da817d8d05f1d8d58f7c901fa7b
SangHun <shzero211@naver.com>
2022-10-17 17:59:45 +0900

    feat: "AuthLevelConverter 를 이용한 enum<->int 기능구현"
    AuthLevelConverter 는
    db 에는 값이 저장되도록 하고
    db 에서 조회시에는 enum 값을 반환

--- 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/member/authority/AuthLevel.java
+++ 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/member/authority/AuthLevel.java
@@ -2,10 +2,15 @@ package com.ebook.multbooks.app.member.authority;
 /**
  * Member 권한 enum
  * */
-public enum AuthLevel  {
+public enum AuthLevel {
     USER(3),AUTHOR(5),ADMIN(7);
-private int level;
-    AuthLevel (int level){
-    this.level=level;
-}
+
+    private final int level; //converter 에서 사용될 변수 값
+
+    AuthLevel(int level){
+        this.level=level;
+    }
+    public int getLevel(){
+            return level;
+    }
 }
--- 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/member/entity/Member.java
+++ 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/member/entity/Member.java
@@ -4,6 +4,7 @@ import com.ebook.multbooks.app.base.entity.BaseEntity;
 import com.ebook.multbooks.app.member.authority.AuthLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
@@ -12,20 +13,22 @@ import javax.persistence.*;
  * */
 @Entity
 @Getter
+@SuperBuilder
 @NoArgsConstructor
 public class Member extends BaseEntity {
 
     @Column(nullable = false)
     private String username;
+
     @Column(nullable = false)
     private String password;
 
-    private String nickname;//작가명 => 등록시 작가회원
+    private String nickname; //작가명 => 등록시 작가회원
 
     @Column(nullable = false)
     private String email;
+
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
     private AuthLevel authLevel;
 
 }
