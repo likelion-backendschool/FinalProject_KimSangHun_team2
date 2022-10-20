6ba41d93ab2140ea6409e6416bdcea9871416681
KimSangHun <40134318+shzero211@users.noreply.github.com>
2022-10-20 13:32:12 +0900

    Revert "1_week_mission"

--- 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/posthashtag/repository/PostHashTagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebook.multbooks.app.posthashtag.repository;
-
-import com.ebook.multbooks.app.posthashtag.entity.PostHashTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostHashTagRepository extends JpaRepository<PostHashTag,Long> {
-}
--- 1_Week_Mission/multbooks/src/main/java/com/ebook/multbooks/app/postkeyword/repository/PostKeywordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebook.multbooks.app.postkeyword.repository;
-
-import com.ebook.multbooks.app.postkeyword.entity.PostKeyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostKeywordRepository extends JpaRepository<PostKeyword,Long> {
-}
